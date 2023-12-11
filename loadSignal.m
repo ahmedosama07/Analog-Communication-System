fe2a51369e54e0c88f809ec514394b351e9af9bf
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-11 02:12:28 +0200

    Update loadSignal.m

--- loadSignal.m
+++ loadSignal.m
@@ -1,15 +1,8 @@
-function [signal, fs, t] = loadSignal(fileName)
+function [signal, fs] = loadSignal(fileName)
 %loadSignal Summary of this function goes here
 %   This function used to load and plot the signal in time domain, it
 %   returns the signal, sampling frequency and time
 
 [signal, fs] = audioread(fileName);
-t = linspace( 0 , length(signal)/fs , length(signal) );
-
-figure(1);
-plot(t,signal);
-xlabel('Time');
-ylabel('Amplitude'); 
-title('The signal in time domain');
 end
 
