0e4f0f961a244bfa2e647b8e2352d189a7e46bfb
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-11 02:22:36 +0200

    Create DSBTC.m

---
+++ DSBTC.m
@@ -0,0 +1,16 @@
+function [modulatedSignal, f] = DSBTC(xm, Fs)
+%DSBTC Summary of this function goes here
+%   This function performs DSBTC modulation for the signal
+A = 2*max(xm) ; 
+S_DSB_TC = xc.*(A+xm) ;
+ 
+figure(5)
+l1 = length(S_DSB_TC);
+f = linspace(-Fs/2,Fs/2,l1) ;
+modulatedSignal = fftshift(fft(S_DSB_TC,l1)) ;
+
+plot(f,abs(modulatedSignal)) ;
+title('DSB-TC MODULATION IN THE FREQUENCY DOMAIN');
+xlabel('frequency(hz)'); ylabel('amplitude');
+end
+
