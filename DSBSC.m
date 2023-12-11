e6ec7b0dcdea297400d4bdabf0b9ae9c88cf212d
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-11 02:42:31 +0200

    Update DSBSC.m

--- DSBSC.m
+++ DSBSC.m
@@ -4,14 +4,13 @@ function [modulatedSignal, f] = DSBSC(xm, xc, Fs)
 S_DSB_SC = xc.*xm ;
 
 figure(6)
-l1l = length(S_DSB_SC);
+l = length(S_DSB_SC);
+f = linspace(-Fs/2,Fs/2,l) ;
+modulatedSignal = fftshift(fft(S_DSB_SC,l)); 
 
-% DSB-SC Signal in the frequency domain
-f = linspace(-Fs/2,Fs/2,l1) ;
-modulatedSignal = fftshift(fft(S_DSB_SC,l1)); 
-
-plot(f,abs(modulatedSignal)) ;
+plot(f,abs(modulatedSignal));
 title('DSB-SC MODULATION IN FREQUENCY DOMAIN');
-xlabel('frequency(hz)'); ylabel('magnitude');
+xlabel('frequency(hz)');
+ylabel('magnitude');
 end
 
