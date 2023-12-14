92e0d7f4b9c29df722acc7fb479d95a89a5f2fba
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-14 02:27:18 +0200

    Update lowPassFilter.m

--- lowPassFilter.m
+++ lowPassFilter.m
@@ -1,23 +1,23 @@
-function [signalFiltered, signalFilteredFreq] = lowPassFilter(signal, signalF,fs)
+function [signalFilteredFreq, signalFilteredTime] = lowPassFilter(signal, signalF,fs)
 %lowPassFilter Summary of this function goes here
 %   The function applies LPF to the signal and returns output filtered
 %   signal in both time and frequency domains
 NoO = ceil(length(signalF)*(8000)/(fs)) ;
 NoZ = floor((length(signalF)-NoO)/2) ;
 LPF = [zeros(NoZ,1); ones(NoO,1); zeros(length(signal)-NoO-NoZ,1)] ;
-signalFiltered=signalF.*LPF;
-signalFilteredFreq=real(ifft(ifftshift(signalFiltered)));
-t=linspace(0,length(signalFilteredFreq)/fs,length(signalFilteredFreq));
+signalFilteredFreq=signalF.*LPF;
+signalFilteredTime=real(ifft(ifftshift(signalFilteredFreq)));
+t=linspace(0,length(signalFilteredTime)/fs,length(signalFilteredTime));
 
 figure(3);
-plot(t,signalFilteredFreq);
+plot(t,signalFilteredTime);
 xlabel('time');
 ylabel('amplitude'); 
 title('Filtered signal in time domain');
 
-magnitudeFiltered = abs(signalFiltered) ;
-phaseFiltered = angle(signalFiltered) ;
-f_received = linspace(-fs/2,fs/2,length(signalFilteredFreq)) ;
+magnitudeFiltered = abs(signalFilteredFreq) ;
+phaseFiltered = angle(signalFilteredFreq) ;
+f_received = linspace(-fs/2,fs/2,length(signalFilteredTime)) ;
 
 figure(4);
 subplot(1,2,1);
