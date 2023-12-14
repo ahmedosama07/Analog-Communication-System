3438d3f6db19aa2ea388fcda68a40bc934506652
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-14 02:27:12 +0200

    Update DSBSC.m

--- DSBSC.m
+++ DSBSC.m
@@ -1,14 +1,15 @@
-function [modulatedSignal, f] = DSBSC(xm, xc, Fs)
+function [modulatedSignalFreq, modulatedSignalTime] = DSBSC(xm, xc, Fs)
 %DSBC Summary of this function goes here
 %   This function performs DSBTC modulation for the signal
 S_DSB_SC = xc.*xm ;
+modulatedSignalTime = S_DSB_SC;
 
-figure(6)
+figure()
 l = length(S_DSB_SC);
 f = linspace(-Fs/2,Fs/2,l) ;
-modulatedSignal = fftshift(fft(S_DSB_SC,l)); 
+modulatedSignalFreq = fftshift(fft(S_DSB_SC,l)); 
 
-plot(f,abs(modulatedSignal));
+plot(f,abs(modulatedSignalFreq));
 title('DSB-SC MODULATION IN FREQUENCY DOMAIN');
 xlabel('frequency(hz)');
 ylabel('magnitude');
