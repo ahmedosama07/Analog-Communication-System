22231d278dbb9e20d83412e2a2292cc95040c30c
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-14 02:27:34 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,26 +1,36 @@
 clc; clear;
+%% Transmitter
 [signal, fs] = loadSignal('eric.wav');
-%sound(signal, fs)
-%pause(8);
+fprintf("Original sound is playing\n");
+sound(signal, fs)
+pause(8);
 [signalFiltered, fs] = transmitterDSB(signal, fs);
 %sound(real(abs(signalFiltered)),fs);
 %pause(8);
 fc = 100000;
 
-[modulatedSignalTC, tTC] = modulator(signalFiltered, fc, 5, fs, "TC");
-[modulatedSignalSC, tSC] = modulator(signalFiltered, fc, 5, fs, "SC");
+[modulatedSignalTC, modulatedSignalTCTime, tTC] = modulator(signalFiltered, fc, 5, fs, "TC");
+[modulatedSignalSC, modulatedSignalSCTime, tSC] = modulator(signalFiltered, fc, 5, fs, "SC");
 
-[envelopeTC] = envelopeDetector(modulatedSignalTC);
-figure(7)
+%% Reciever using ED
+[envelopeTC] = envelopeDetector(modulatedSignalTCTime);
+figure(14)
 plot(tTC,envelopeTC);
-title('DSB-TC DEMODULATION IN THE TIME DAOMAIN USING ENVELOPE');
+title('DSB-TC DEMODULATION IN THE TIME DOMAIN USING ENVELOPE');
 xlabel('time ');
 ylabel('amplitude');
 
-[envelopeSC] = envelopeDetector(modulatedSignalSC);
+envelopeTC=resample(envelopeTC, fc, 5*fc) ;
+sound(envelopeTC, fc);
+pause(8);
+
+[envelopeSC] = envelopeDetector(modulatedSignalSCTime);
 figure(8)
 plot(tSC,envelopeSC);
-title('DSB-SC DEMODULATION IN TIME DAOMAIN USING ENVELOPE');
+title('DSB-SC DEMODULATION IN TIME DOMAIN USING ENVELOPE');
 xlabel('time ');
 ylabel('amplitude');
+envelopeSC=resample(envelopeSC, fc,5*fc) ;
+sound(envelopeTC, fc);
+pause(8);
 
