e38c5007c11d2b5fc4aa8c423f07ab88796148c6
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-20 12:37:25 +0200

    Update modulatorDSB.m

--- modulatorDSB.m
+++ modulatorDSB.m
@@ -12,7 +12,46 @@ xm = resampled_signal ;
 
 if samplingType == "TC"
     [modulatedSignalFreq, modulatedSignalTime] = DSBTC(xm, xc, Fs);
+    figure()
+    plot(t, modulatedSignalTime);
+    title('DSB-TC MODULATION IN TIME DOMAIN');
+    xlabel('time');
+    ylabel('magnitude');
+    saveas(gcf,'figures\Exp1\DSB-TC Modulated Signal - Time.png')
+
+    figure()
+    subplot(2, 1, 1);
+    plot(f,abs(modulatedSignalFreq));
+    title('DSB-TC MODULATION MAGNITUDE IN FREQUENCY DOMAIN');
+    xlabel('frequency(hz)');
+    ylabel('magnitude');
+    subplot(2, 1, 2);
+    plot(f,angle(modulatedSignalFreq));
+    title('DSB-TC MODULATION PHASE IN FREQUENCY DOMAIN');
+    xlabel('frequency(hz)');
+    ylabel('phase');
+    saveas(gcf,'figures\Exp1\DSB-TC Modulated Signal - Frequency.png')
 elseif samplingType == "SC"
     [modulatedSignalFreq, modulatedSignalTime] = DSBSC(xm, xc, Fs);
+    figure()
+    plot(t, modulatedSignalTime);
+    title('DSB-SC MODULATION IN TIME DOMAIN');
+    xlabel('time');
+    ylabel('magnitude');
+    saveas(gcf,'figures\Exp1\DSB-SC Modulated Signal - Time.png')
+
+    figure()
+    subplot(2, 1, 1);
+    plot(f,abs(modulatedSignalFreq));
+    title('DSB-SC MODULATION MAGNITUDE IN FREQUENCY DOMAIN');
+    xlabel('frequency(hz)');
+    ylabel('magnitude');
+    subplot(2, 1, 2);
+    plot(f,angle(modulatedSignalFreq));
+    title('DSB-SC MODULATION PHASE IN FREQUENCY DOMAIN');
+    xlabel('frequency(hz)');
+    ylabel('phase');
+    saveas(gcf,'figures\Exp1\DSB-SC Modulated Signal - Frequency.png')
+    
 end
 
