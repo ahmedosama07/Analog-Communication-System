1d47500a6489b2c2c3601dc398e157a79a7dfcd3
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-12-23 17:52:16 +0200

    Update DSBSC.m

--- DSBSC.m
+++ DSBSC.m
@@ -8,11 +8,5 @@ figure()
 l = length(S_DSB_SC);
 f = linspace(-Fs/2,Fs/2,l) ;
 modulatedSignalFreq = fftshift(fft(S_DSB_SC,l)); 
-
-plot(f,abs(modulatedSignalFreq));
-title('DSB-SC MODULATION IN FREQUENCY DOMAIN');
-xlabel('frequency(hz)');
-ylabel('magnitude');
-saveas(gcf,'figures\Exp1\DSB-SC Modulated Signal - Frequency Domain.png')
 end
 
